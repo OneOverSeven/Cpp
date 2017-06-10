6a24735f32a85e93748ed0e09455c3c0364c959f
Maxime Logé <max.loge@gmail.com>
2017-06-10 14:51:06 +0200

    wxc

--- project1/main.cpp
+++ project1/main.cpp
@@ -1,34 +1,47 @@
 #include "std_lib_facilities.h"
 int main()
 {
-          cout << "Please enter your first name and age\n";
-          string first_name = "?!?";          // string variable
-          int age = 666;
-		  int var1= 777;
-		  int var2;
-		  int var3;
-		  int var4;
-		  cout << "var4 = " << var4 << ".\n";
-		  var4>>var1;
-		  cout << "var4 = " << var4 << ".\n";
-		  cout << "var1 = " << var1 << ".\n";
-		  var2<<var1;
-		  cout << "var1 = " << var1 << ".\n";
-		  var3>>var1;
-		  cout << "var1 = " << var1 << ".\n";
-		  cout << "var2 = " << var2 << ".\n";
-		  cout << "var3 = " << var3 << ".\n";
-          cout << "var4 = " << var4 << ".\n";
-          cout << "Please enter a floating-point value: ";
-          double n;
-          cin >> n;
-          cout << "n == " << n
-                    << "\nn+1 == " << n+1
-                    << "\nthree times n == " << 3*n
-                    << "\ntwice n == " << n+n
-                    << "\nn squared == " << n*n
-                    << "\nhalf of n == " << n/2
-                    << "\nsquare root of n == " << sqrt(n)
-                    << "\n";  // another name for newline (“end of line”) in output
-          return 0;
+    cout << "Please enter your first name and age\n";
+    string first_name = "?!?";          // string variable
+    int age = 666;
+	int var1= 777;
+	int var2;
+	int var3;
+	int var4;
+	cout << "var4 = " << var4 << ".\n";
+	var4>>var1;
+	cout << "var4 = " << var4 << ".\n";
+	cout << "var1 = " << var1 << ".\n";
+	var2<<var1;
+	cout << "var1 = " << var1 << ".\n";
+	var3>>var1;
+	cout << "var1 = " << var1 << ".\n";
+	cout << "var2 = " << var2 << ".\n";
+	cout << "var3 = " << var3 << ".\n";
+    cout << "var4 = " << var4 << ".\n";
+    cout << "Please enter a floating-point value: ";
+    double n;
+    cin >> n;
+    cout << "n == " << n
+            << "\nn+1 == " << n+1
+            << "\nthree times n == " << 3*n
+            << "\ntwice n == " << n+n
+            << "\nn squared == " << n*n
+            << "\nhalf of n == " << n/2
+            << "\nsquare root of n == " << sqrt(n)
+            << "\n";  // another name for newline (“end of line”) in output
+
+    string word1="alpha";
+    word1 = word1 + "beta!";
+	cout << "word1=" << word1 << ".\n";
+
+	string previous = " ";       // previous word; initialized to “not a word”
+    string current;                             // current word
+    while (cin>>current) {// read a stream of words
+        if (previous == current)    // check if the word is the same as last
+            cout << "repeated word: " << current << "\n";
+            previous = current;
+        }
+
+        return 0;
 }
