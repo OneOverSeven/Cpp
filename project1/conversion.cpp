528ac0e415500cba99fa82b85b13a76985dc2bba
OneOverSeven <max.loge@gmail.com>
2017-06-11 03:33:59 +0200

    ùml

---
+++ project1/conversion.cpp
@@ -0,0 +1,14 @@
+#include "std_lib_facilities.h"
+int main2()
+{
+double d = 0;
+while (cin >> d)    // repeat the statements below
+{                   // as long as we type in numbers
+    int i=d;        // squeezing a double into an int
+    char c=i;       // squeezing an int into a char
+    int i2 = c;     //get the integer value of the character
+    cout << "d == " << d << "\ni== " << i << "\ni2 == " << i2 << "char(" << c << ")\n";
+}
+        return 0;
+}
+
